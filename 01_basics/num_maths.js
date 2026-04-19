73edbc9b117a310b65fd8ce6ef3fae88415f018c
Ayush Kumar Tiwari <aayushtiwari0015@gmail.com>
2026-04-19 16:22:42 +0000

    num and maths

---
+++ 01_basics/num_maths.js
@@ -0,0 +1,19 @@
+const score =400
+console.log(score)
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString());
+
+console.log (typeof balance);
+console.log(balance.toFixed(2));
+
+const otherNumber = 123.896
+
+console.log(otherNumber.toPrecision(4))
+
+
+const hundereds =1000000
+
+console.log(hundereds.toLocaleString('en-IN'))
