678c0286284e0df38a9d6b7e4dff4b3d9705237b
Ayush Kumar Tiwari <aayushtiwari0015@gmail.com>
2026-05-04 22:09:00 +0000

    03_arrow

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,48 @@
+const user = {
+    username:"Ayush",
+    price :999 ,
+    
+    welcomeMessage:function(){
+        console.log(`${this.username} , Welcome to website`);
+        console.log(this);
+        
+        
+    }
+}
+// user.welcomeMessage()
+// user.username ="sam"
+// user.welcomeMessage()
+// console.log(this);
+
+
+// function chai(){
+//       let username ="Ayush"
+//       console.log(this.username);
+      
+// }
+// chai()
+// const chai = function(){
+//     let username ="Ayush"
+//       console.log(this.username);
+
+// }
+// chai()
+
+// const chai =() => {
+//     let username ="Ayush"
+//       console.log(this.username);
+
+// }
+// chai()
+
+// const addTwo = (num1 ,num2) =>{
+//      return num1+num2
+// }
+// console.log(addTwo(3 ,4));
+
+// const addTwo = (num1 ,num2) =>num1+num2
+// const addTwo = (num1 ,num2) =>(num1+num2)
+const addTwo = (Num1 , num2) =>({username:"Ayush"})
+console.log(addTwo(3 ,4));
+
+
